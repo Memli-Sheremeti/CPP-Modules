f14bb56e25302abfa89634c1711b5c0cccbb1ce3
Memli Sheremeti <mshereme@student.42.fr>
2024-03-07 12:14:32 +0100

    CPP00 - ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -5,14 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/19 11:12:14 by mshereme          #+#    #+#             */
-/*   Updated: 2024/02/19 11:13:33 by mshereme         ###   ########.fr       */
+/*   Created: 2024/03/07 11:47:39 by mshereme          #+#    #+#             */
+/*   Updated: 2024/03/07 11:51:39 by mshereme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <algorithm>
+#include <string>
 
-int	main(void)
+int	main(int ac, char **av)
 {
-	std::cout << "Hello world!\n";
+	if (ac > 1)
+	{
+		for (int i = 1; i < ac; i++)
+		{
+			std::string str = av[i];
+			std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+			std::cout << str;
+		}
+		std::cout << std::endl;
+	}
+	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	return (0);
 }
