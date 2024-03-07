6a0e9cbf20519c79d1d1525167ef6e1a04d94385
Memli Sheremeti <mshereme@student.42.fr>
2024-03-07 13:49:09 +0100

    CPP00 - ex00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -6,22 +6,29 @@
 /*   By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 11:47:39 by mshereme          #+#    #+#             */
-/*   Updated: 2024/03/07 11:51:39 by mshereme         ###   ########.fr       */
+/*   Updated: 2024/03/07 13:48:18 by mshereme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <algorithm>
 #include <string>
+
+void	ft_toupper_str(std::string &str)
+{
+	for (std::string::iterator it = str.begin(); it!=str.end(); it++)
+		*it = toupper(*it);
+}
 
 int	main(int ac, char **av)
 {
+	std::string str;
+
 	if (ac > 1)
 	{
 		for (int i = 1; i < ac; i++)
 		{
-			std::string str = av[i];
-			std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+			str = av[i];
+			ft_toupper_str(str);
 			std::cout << str;
 		}
 		std::cout << std::endl;
