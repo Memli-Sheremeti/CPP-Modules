5921f6e166eae1042fe16b4b9a41a3247ac11dda
Memli <memli@iMac-de-Annette.lan>
2024-03-18 18:24:49 +0100

    CPP03 / ex01

---
+++ 03/ex01/ClapTrap.cpp
@@ -0,0 +1,109 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 13:59:59 by mshereme          #+#    #+#             */
+/*   Updated: 2024/03/18 16:36:12 by mshereme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+void	ClapTrap::attack( const std::string& target)
+{
+	if (this->_energy_pts && this->_hit_pts)
+	{
+		std::cout << "ClapTrap "
+		<< this->_name << " attacks " << target << ", causing "
+		<< this->_att_dmg << " points of damage!" << std::endl;
+		this->_energy_pts -= 1;
+		return ;
+	}
+	else if (!this->_hit_pts)
+	{
+		std::cout << "ClapTrap "
+		<< this->_name << " couldn't attack " << target << ", no sufficient health !" << std::endl;
+		return ;
+	}
+	else if (!this->_energy_pts)
+	{
+		std::cout << "ClapTrap "
+		<< this->_name << " couldn't attack " << target << ", no sufficient energy !" << std::endl;
+		return ;
+	}
+}
+
+void	ClapTrap::takeDamage( unsigned int amout )
+{
+	std::cout << "ClapTrap "
+	<< this->_name << " looses " << amout << " of life..." << std::endl;
+	amout > this->_hit_pts ? this->_hit_pts = 0 : this->_hit_pts -= amout;
+	if (!this->_hit_pts)
+	std::cout << "ClapTrap "
+	<< this->_name << " is dead... " << std::endl;
+	return ;
+}
+void	ClapTrap::beRepaired( unsigned int amout )
+{
+	if (this->_energy_pts && this->_hit_pts)
+	{
+		std::cout << "ClapTrap "
+		<< this->_name << " repaired itslef of " << amout << " !!!" << std::endl;
+		this->_energy_pts -= 1;
+		this->_hit_pts += amout;
+		std::cout << "ClapTrap "
+		<< this->_name << " has " << this->_hit_pts << " point of life." << std::endl;
+		return ;
+	}
+	else if (!this->_hit_pts)
+	{
+		std::cout << "ClapTrap "
+		<< this->_name << " couldn't repaired itself" << ", no sufficient health !" << std::endl;
+		return ;
+	}
+	else if (!this->_energy_pts)
+	{
+		std::cout << "ClapTrap "
+		<< this->_name << " couldn't repaired itself" << ", no sufficient energy !" << std::endl;
+		return ;
+	}
+	return ;
+}
+
+ClapTrap & ClapTrap::operator=(const ClapTrap &obj)
+{
+	if (CONS)
+		std::cout << "Copy constructor called" << std::endl;
+	if (this != &obj)
+	{
+		this->_name = obj._name;
+		this->_hit_pts = obj._hit_pts;
+		this->_energy_pts = obj._energy_pts;
+		this->_att_dmg = obj._att_dmg;
+	}
+	return (*this);
+}
+
+ClapTrap::ClapTrap( std::string name ) : _name(name), _hit_pts(10), _energy_pts(10), _att_dmg(0)
+{
+	if (CONS)
+		std::cout << "Clap Trap Constructor called" << std::endl;
+	return ;
+}
+
+ClapTrap::ClapTrap( void ) : _name("DEFAULT CP"), _hit_pts(10), _energy_pts(10), _att_dmg(0)
+{
+	if (CONS)
+		std::cout << "Clap Trap Default constructor called" << std::endl;
+	return ;
+}
+
+ClapTrap::~ClapTrap( void )
+{
+	if (CONS)
+		std::cout << "Destructor called" << std::endl;
+	return ;
+}
---
+++ 03/ex01/main.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/12 14:11:26 by mshereme          #+#    #+#             */
+/*   Updated: 2024/03/18 18:01:02 by mshereme         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+#include <iostream>
+
+int main( void )
+{
+	std::cout << "--- Waking up\n" << std::endl;
+	ClapTrap claptrap("Bishop");
+	ScavTrap scavtrap("PAWNS");
+
+	std::cout << "\n--- Attacks\n" << std::endl;
+	claptrap.attack("The King");
+	claptrap.attack("The Queen");
+	scavtrap.attack("The King");
+	scavtrap.attack("The Queen");
+
+	std::cout << "\n--- Take damage and be repaired\n" << std::endl;
+	claptrap.takeDamage(2);
+	claptrap.beRepaired(2);
+	scavtrap.takeDamage(2);
+	scavtrap.beRepaired(2);
+	// Energy = 6
+
+	std::cout << std::endl;
+	claptrap.takeDamage(3);
+	claptrap.beRepaired(1);
+	scavtrap.takeDamage(3);
+	scavtrap.beRepaired(1);
+	
+	// Energy = 4
+
+	std::cout << std::endl;
+	claptrap.takeDamage(1);
+	claptrap.beRepaired(10);
+	claptrap.attack("Tower");
+	scavtrap.takeDamage(1);
+	scavtrap.beRepaired(10);
+	scavtrap.attack("Tower");
+	// Energy = 1
+
+	std::cout << std::endl;
+	claptrap.takeDamage(4);
+	claptrap.beRepaired(10);
+	claptrap.attack("King");
+	scavtrap.takeDamage(4);
+	scavtrap.beRepaired(10);
+	scavtrap.attack("King");
+
+	// Energy = 0 --> not enought
+
+	std::cout << "\n--- Taking a lot of damage\n" << std::endl;
+	claptrap.takeDamage(10000);
+	scavtrap.takeDamage(10000);
+
+
+	std::cout << "\n--- NO LIFE OR ENERGY \n" << std::endl;
+	claptrap.attack("Knight");
+	claptrap.beRepaired(100);
+	scavtrap.attack("Knight");
+	scavtrap.beRepaired(100);
+
+
+	return (0);
+}
