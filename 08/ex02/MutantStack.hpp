8b5a35c4b46985a82c2347f554364c5f83712146
Memli Sheremeti <mshereme@student.42.fr>
2024-04-18 18:27:46 +0200

    CPP

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -6,7 +6,7 @@
 /*   By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:53:11 by mshereme          #+#    #+#             */
-/*   Updated: 2024/04/17 17:27:34 by mshereme         ###   ########.fr       */
+/*   Updated: 2024/04/18 17:11:02 by mshereme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@
 # include <iostream>
 # include <stack>
 # include <algorithm>
-# include <limits>
-# include <cmath>
 # include <iterator>
 
 template <typename T>
@@ -30,7 +28,38 @@ class MutantStack : public std::stack<T>
 	MutantStack( MutantStack const & obj );
 	MutantStack & operator=( MutantStack const & obj );
 
-	class Iter
+	class iterator : public std::iterator<
+								std::bidirectional_iterator_tag,
+								T,
+								std::ptrdiff_t,
+								T *,
+								T &
+								>
+	{
+		private :
+
+		T * m_ptr;
+
+		public :
+
+		iterator( void ) {}
+		~iterator( void ) {}
+		iterator( T* ptr) : m_ptr(ptr) {}
+		T & operator*() const { return ( *m_ptr ); }
+		T * operator->() { return ( m_ptr ); }
+		iterator & operator++() { m_ptr--; return (*this); }
+		iterator operator++( int ) { iterator old = *this; ++(*this); return (old); }
+
+		iterator & operator--() { m_ptr++; return (*this); }
+		iterator operator--( int ) { iterator old = *this; --(*this); return (old); }
+
+		bool operator==(iterator& obj) { return ( *m_ptr == *(obj.m_ptr) ? true : false); };
+		bool operator!=(iterator& obj) { return ( *m_ptr != *(obj.m_ptr) ? true : false); };
+
+	};
+
+	iterator begin() { return iterator( &this->c.back() ); };
+	iterator end() { return iterator( &this->c.front() - 1 ); };
 };
 
 #endif
--- 08/ex02/main.cpp
+++ 08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mshereme <mshereme@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 12:15:56 by mshereme          #+#    #+#             */
-/*   Updated: 2024/04/15 15:06:33 by mshereme         ###   ########.fr       */
+/*   Updated: 2024/04/18 17:11:43 by mshereme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,20 @@ int main()
 {
 	MutantStack<int> mstack;
 
-	mstack.push(5);
-	mstack.push(17);
+	mstack.push(1);
+	mstack.push(2);
 	std::cout << mstack.top() << std::endl;
 	mstack.pop();
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
+	mstack.push(4);
 	mstack.push(5);
-	mstack.push(737);
-	//[...]
-	mstack.push(0);
+	mstack.push(6);
+
+
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+
 	++it;
 	--it;
 	while (it != ite)
@@ -35,6 +37,7 @@ int main()
 		std::cout << *it << std::endl;
 		++it;
 	}
+
 	std::stack<int> s(mstack);
 	return 0;
 }
